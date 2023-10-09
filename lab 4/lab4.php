ebe9a38308626e5e9f4d7103af1b71e3fd2516b2
Md. Zamiul Sadik Nahin <120377942+Zsadik14@users.noreply.github.com>
2023-10-09 12:17:55 +0600

    Add files via upload

---
+++ lab 4/lab4.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+    <h1>Profile</h1>
+    <table>
+
+        <tr >
+            <td>
+    
+                <fieldset>
+                    <legend>General Information</legend>
+                    <table>
+                        <tr>
+                            <td>
+                               <p><b>&nbsp;&nbsp;&nbsp;First Name &nbsp;&nbsp;&nbsp;:</b>Mir Md.</p>
+                               <p><b>&nbsp;&nbsp;&nbsp;Last Name &nbsp;&nbsp;&nbsp;&nbsp;:</b>Kawsar</p>
+                               <p><b>Father's Name&nbsp;&nbsp;:</b>XYZ</p>
+                               <p><b>Mother's Name&nbsp;:</b>ZYX</p>
+                               <p><b>&nbsp;&nbsp;&nbsp;Blood Group&nbsp;&nbsp;:</b>B+</p>
+                               <p><b>&nbsp;&nbsp;&nbsp;Religion&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</b>Islam</p>
+                
+                            </td>
+                            <td>
+                                <img src="0011.jpg" alt="A picture of a profile icon" height="150px" width="100px">
+                            </td>
+                        </tr>
+                    </table>
+                </fieldset>
+            </td>
+            <td>
+                <fieldset>
+                    <legend>Contact Information</legend>
+                
+                       <table>
+                        <tr >
+                            <td rowspan="2" colspan="2">
+                            <p><b>&nbsp;&nbsp;&nbsp;&nbsp;Email &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</b> kawsar@aiub.edu</p>
+                            <p><b>Phone/Mobile &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</b>+0181*******</p>
+                            <p><b>&nbsp;&nbsp;&nbsp;&nbsp;Website &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</b>http://kawsar.me</p>
+                            <p><b>Present Address &nbsp;:</b>408, Kuratoli, Khilkhet, Dhaka 1229,Bangladesh</p>
+                            </td>
+                        </tr>
+
+                       </table>
+                    
+                </fieldset>
+            </td>
+        </tr>
+
+    </table>
+    
+</body>
+</html>
